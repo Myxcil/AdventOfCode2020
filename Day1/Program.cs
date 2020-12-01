0bb64d2ae3058f874c025599ce0bad9e0a7a7416
Markus Kark <markus.kark@gmail.com>
2020-12-01 12:42:49 +0100

    Day 1 finished

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 //----------------------------------------------------------------------------------------------------------------------------------------
 namespace Day1
@@ -9,7 +12,58 @@ namespace Day1
         //--------------------------------------------------------------------------------------------------------------------------------
         static void Main(string[] args)
         {
+            //RunPartOne();
+            RunPartTwo();
+        }
 
+        //--------------------------------------------------------------------------------------------------------------------------------
+        /*
+         * Find the two entries that sum to 2020; what do you get if you multiply them together?
+         */
+        //--------------------------------------------------------------------------------------------------------------------------------
+        private static void RunPartOne()
+        {
+            int[] values = File.ReadAllLines("input.txt").Select(l => int.Parse(l)).ToArray();
+            for (int i = 0; i < values.Length - 1; ++i)
+            {
+                int first = values[i];
+                for (int j = i + 1; j < values.Length; ++j)
+                {
+                    int second = values[j];
+                    if ((first + second) == 2020)
+                    {
+                        Console.WriteLine("{0}", first * second);
+                        return;
+                    }
+                }
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------------------------------
+        /*
+         * What is the product of the three entries that sum to 2020?
+         */
+        //--------------------------------------------------------------------------------------------------------------------------------
+        private static void RunPartTwo()
+        {
+            int[] values = File.ReadAllLines("input.txt").Select(l => int.Parse(l)).ToArray();
+            for (int i = 0; i < values.Length - 2; ++i)
+            {
+                int first = values[i];
+                for (int j = i + 1; j < values.Length - 1; ++j)
+                {
+                    int second = values[j];
+                    for (int k = j + 1; k < values.Length; ++k)
+                    {
+                        int third = values[k];
+                        if ((first+second+third) == 2020)
+                        {
+                            Console.WriteLine("{0}", first * second * third);
+                            return;
+                        }
+                    }
+                }
+            }
         }
     }
 }
